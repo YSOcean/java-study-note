b59d4fe184d0dd84fa9f655e8bccf037b4d51b87
yushuai <yushuai@shimo.im>
2021-08-16 22:44:49 +0800

    Initial commit

---
+++ src/main/java/com/javanorth/JavaStudyNoteApplication.java
@@ -0,0 +1,13 @@
+package com.javanorth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaStudyNoteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaStudyNoteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/javanorth/JavaStudyNoteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javanorth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaStudyNoteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
